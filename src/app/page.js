01c0c366102e9e1134d947b5c7604c7b4f4348b5
bwsrcurtis <bwsrcurtis@gmail.com>
2023-05-05 19:04:32 -0400

    Initial changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,7 @@
-import Hero from './components/Hero';
-import Examples from './components/Examples';
-import Offerings from './components/Offerings';
-import GetStarted from './components/GetStarted';
 
 export default function Home() {
   return (
-    <main className="bg-secondary min-h-[calc(100vh-112px)]">
-      <Hero></Hero>
-      <Offerings></Offerings>
-      <Examples></Examples>
-      <GetStarted></GetStarted>
+    <main className="bg-secondary">
     </main>
   );
 }
