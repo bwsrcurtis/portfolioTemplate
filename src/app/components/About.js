d6a6cec9796efe60bfe746aeace87e4e90da2d11
bwsrcurtis <bwsrcurtis@gmail.com>
2023-05-06 01:34:21 -0400

    Tweaks

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -9,18 +9,22 @@ const About = () => {
          border-2 border-text border-solid'></Image>
       <div className='mx-auto w-full xl:w-[calc(50%-3.25rem)]'>
         <p className='leading-none tracking-tight text-5xl xl:text-6xl text-left'>
-          Hi there, I&apos;m a
-          <span className='group-hover:text-tertiary transition delay-75 duration-1000'> designer </span>
-          with a passion for creating meaningful and
-          <span className='group-hover:text-secondary transition delay-150 duration-1000'> impactful </span>
-          visual experiences.
+          Hi there, I&apos;m a&nbsp;
+          <span className='group-hover:text-tertiary group-hover:bg-primary/75
+          transition delay-75 duration-1000'>designer</span>
+          &nbsp;with a passion for creating meaningful and&nbsp;
+          <span className='group-hover:text-secondary group-hover:bg-primary/75
+          transition delay-150 duration-1000'>impactful</span>
+          &nbsp;visual experiences.
           <br></br>
           <br></br>
-          Design is not just about
-          <span className='group-hover:text-secondary transition delay-300 duration-1000'> aesthetics, </span>
-          but also about solving
-          problems and communicating messages
-          <span className='group-hover:text-tertiary transition delay-[600ms] duration-1000'> effectively.</span>
+          Design is not just about&nbsp;
+          <span className='group-hover:text-secondary group-hover:bg-primary/75
+          transition delay-300 duration-1000'>aesthetics,</span>
+          &nbsp;but also about solving
+          problems and communicating messages&nbsp;
+          <span className='group-hover:text-tertiary group-hover:bg-primary/75
+          transition delay-[600ms] duration-1000'>effectively.</span>
 
         </p> </div>
     </div>
