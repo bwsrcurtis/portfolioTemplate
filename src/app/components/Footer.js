17b063649e204f930b78f9e5243a1d0b716fb349
bwsrcurtis <bwsrcurtis@gmail.com>
2023-07-05 23:57:19 -0400

    Unused import

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,7 +1,6 @@
 'use client';
 
-import matter from 'gray-matter'; import React from 'react';
-
+import matter from 'gray-matter';
 const Footer = async () => {
 	const content = await import('../../../content/pages/Footer/Footer.md');
 	const local = matter(content.default);
